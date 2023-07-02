3c05fc65d52e67387a30f6e729daf104087f5cb1
Frans <frans_421_12@hotmail.com>
2023-07-01 22:57:15 -0500

    feat: Diseño de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
-
+const routes: Routes = [
+  {path: '', pathMatch:'full', redirectTo:'add-employee'},
+  {path: 'add-employee', component:AddEmployeeComponent},
+  {path: 'edit-employee/:id', component:EditEmployeeComponent},
+  {path: 'list-employee', component:ListEmployeeComponent}
+];
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
-  ]
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
